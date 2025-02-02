4487a57976787bfade166a4ea9535b4f006c6e68
Ekaterina Deribina <dekatei@yandex.ru>
2025-02-02 20:00:59 +0300

    pathJoin

--- main.go
+++ main.go
@@ -3,25 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//"path/filepath"
+	"path/filepath"
 )
 
 func mainHandle(res http.ResponseWriter, req *http.Request) {
+	var filePath string
 
-	if req.URL.Path == "" {
-		http.ServeFile(res, req, "./web/index.html")
+	if req.URL.Path == "/" {
+		filePath = filepath.Join("web", "index.html")
 	} else {
-		http.ServeFile(res, req, "./web"+req.URL.Path)
+		filePath = filepath.Join("web", req.URL.Path)
 	}
+	http.ServeFile(res, req, filePath)
 
 }
 
 func main() {
 	fmt.Println("Запускаем сервер")
-	http.HandleFunc(`/`, mainHandle)
+	/*http.HandleFunc(`/`, mainHandle)
 	err := http.ListenAndServe(":7540", nil)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Завершаем работу")
+	fmt.Println("Завершаем работу")*/
+	go func() {
+		err := http.ListenAndServe(":7540", nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
